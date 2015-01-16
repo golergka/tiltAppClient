906fdacc8e7a8684df11b030178ed549867afcc4
Max Yankov <golergka@gmail.com>
2015-01-16 15:50:11 +0200

    Loads google in a webview

--- app/src/main/java/com/example/golergka/playimtest/MainActivity.java
+++ app/src/main/java/com/example/golergka/playimtest/MainActivity.java
@@ -3,15 +3,28 @@ package com.example.golergka.playimtest;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    WebView mainWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainWebView = (WebView) findViewById(R.id.webView);
+        mainWebView.getSettings().setJavaScriptEnabled(true);
+        mainWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        mainWebView.loadUrl("http://google.com");
     }
 
 
@@ -20,20 +33,5 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
