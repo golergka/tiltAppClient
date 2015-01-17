075b8233a643caf5ff48eb7dbf382b44100f3209
Max Yankov <golergka@gmail.com>
2015-01-17 17:02:13 +0200

    Final release

--- app/src/main/java/com/example/golergka/playimtest/MainActivity.java
+++ app/src/main/java/com/example/golergka/playimtest/MainActivity.java
@@ -8,17 +8,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.webkit.JavascriptInterface;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
-
 public class MainActivity extends ActionBarActivity {
-
-    WebView mainWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +23,6 @@ public class MainActivity extends ActionBarActivity {
 
         // Getting the tilt data
         final SensorManager sensorManager = (SensorManager) this.getSystemService(SENSOR_SERVICE);
-        final float[] magnet        = new float[3];
         final float[] acceleration  = new float[3];
 
         sensorManager.registerListener(
@@ -44,22 +39,8 @@ public class MainActivity extends ActionBarActivity {
                 SensorManager.SENSOR_DELAY_NORMAL
         );
 
-        sensorManager.registerListener(
-                new SensorEventListener() {
-                    @Override
-                    public void onSensorChanged(SensorEvent event) {
-                        System.arraycopy(event.values, 0, magnet, 0, 3);
-                    }
-
-                    @Override
-                    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
-                },
-                sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
-                SensorManager.SENSOR_DELAY_NORMAL
-        );
-
         // Creating the web view
-        mainWebView = (WebView) findViewById(R.id.webView);
+        final WebView mainWebView = (WebView) findViewById(R.id.webView);
         mainWebView.getSettings().setJavaScriptEnabled(true);
         mainWebView.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -70,17 +51,14 @@ public class MainActivity extends ActionBarActivity {
         mainWebView.loadUrl("http://golergka.github.io/tiltAppServer/");
 
         // Setting up the API
-        mainWebView.addJavascriptInterface(new Object() {
+        final Object orientationProvider = new Object() {
             @JavascriptInterface
             public String getOrientation() throws JSONException {
-                final float[] rotation = new float[9];
-                SensorManager.getRotationMatrix(rotation, null, acceleration, magnet);
-                final float[] orientation = new float[3];
-                SensorManager.getOrientation(rotation, orientation);
-                return new JSONArray(orientation).toString();
+                return new JSONArray(acceleration).toString();
             }
-        },
-        "OrientationProvider");
+        };
+        mainWebView.addJavascriptInterface(orientationProvider, "OrientationProvider");
+
     }
 
 
